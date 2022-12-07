a4ee8eddddd74b0df1811e963278e3c3058416f0
Jalinson Diaz <zjaaaldev@gmail.com>
2022-12-07 16:02:33 -0300

    added test input

--- adventOfCode/day7/input.js
+++ adventOfCode/day7/input.js
@@ -1,4 +1,4 @@
-export default `$ cd /
+export const input = `$ cd /
 $ ls
 115454 dwdllt
 dir jmdf
@@ -1017,3 +1017,27 @@ dir przn
 $ cd przn
 $ ls
 167375 npscjw.ndb`;
+
+export const testInput = `$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k`;
