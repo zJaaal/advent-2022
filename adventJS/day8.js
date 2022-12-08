c05230f5743fd5a2528ef6ffb494aa67b128ed12
Jalinson Diaz <zjaaaldev@gmail.com>
2022-12-08 15:25:50 -0300

    trying to change the hacky solution

--- adventJS/day8.js
+++ adventJS/day8.js
@@ -1,11 +1,22 @@
+// Hacky solution
+// function checkPart(part) {
+//   if (new Set([...part.split('')]).size == part.length) return false;
+//   return [0, 2, part.length].includes(
+//     part.split('').reduce((acc, curr, i, array) => {
+//       if (curr !== array[array.length - i - 1]) acc.push(curr);
+//       return acc;
+//     }, []).length
+//   );
+// }
+
+// Trying to fix it
 function checkPart(part) {
-  if (part == 'midu') return false;
-  return [0, 2, part.length].includes(
-    part.split('').reduce((acc, curr, i, array) => {
-      if (curr !== array[array.length - i - 1]) acc.push(curr);
+  let partArray = part.split('');
+  return [0, 1].includes(
+    partArray.splice(0, part.length / 2).reduce((acc, curr, i) => {
+      if (partArray.includes(curr) == i) acc.push(curr);
       return acc;
     }, []).length
   );
 }
-
-console.log(checkPart('myolooloy'));
+console.log(checkPart('ymolomoloy')); // > true (should be false)
