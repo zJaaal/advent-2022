c9002ae1a8edddd9429a6ac15e4a9c32228389a8
Jalinson Diaz <zjaaaldev@gmail.com>
2022-12-10 03:07:35 -0300

    trying to solve it

--- adventOfCode/day9/part2/solution.js
+++ adventOfCode/day9/part2/solution.js
@@ -9,23 +9,52 @@ The same as part 1 but with 9 knots
 let calculateDistance = (x1, x2, y1, y2) =>
   Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
 
-let headMovement = {
+const headMovement = {
   U: (lastPosition) => [lastPosition[0], lastPosition[1] + 1],
   D: (lastPosition) => [lastPosition[0], lastPosition[1] - 1],
   R: (lastPosition) => [lastPosition[0] + 1, lastPosition[1]],
   L: (lastPosition) => [lastPosition[0] - 1, lastPosition[1]],
 };
 
-let tailMovement = {
+const headTailMovement = {
   U: (headPosition) => [headPosition[0], headPosition[1] - 1],
   D: (headPosition) => [headPosition[0], headPosition[1] + 1],
   R: (headPosition) => [headPosition[0] - 1, headPosition[1]],
   L: (headPosition) => [headPosition[0] + 1, headPosition[1]],
 };
 
+//Not working right, looks like is not going in tempo and lose reference again
+let knotMovement = {
+  U: (headKnotPosition, tailKnotPosition, distance) => {
+    if (distance == 2.23606797749979)
+      //If is on diagonal, go to where it was
+      return [headKnotPosition[0], headKnotPosition[1]];
+
+    return [headKnotPosition[0], tailKnotPosition[1] + 1]; // Else move to where you should be
+  },
+  D: (headKnotPosition, tailKnotPosition, distance) => {
+    if (distance == 2.23606797749979)
+      return [headKnotPosition[0], headKnotPosition[1]];
+
+    return [headKnotPosition[0], tailKnotPosition[1] - 1];
+  },
+  R: (headKnotPosition, tailKnotPosition, distance) => {
+    if (distance == 2.23606797749979)
+      return [headKnotPosition[0], headKnotPosition[1]];
+
+    return [tailKnotPosition[0] + 1, headKnotPosition[1]];
+  },
+  L: (headKnotPosition, tailKnotPosition, distance) => {
+    if (distance == 2.23606797749979)
+      return [headKnotPosition[0], headKnotPosition[1]];
+
+    return [tailKnotPosition[0] - 1, headKnotPosition[1]];
+  },
+};
+
 function solution(input) {
-  let arraySim = Array.from({ length: 29 }, (x) =>
-    Array.from({ length: 29 }, (x) => '.')
+  let arraySim = Array.from({ length: 30 }, (x) =>
+    Array.from({ length: 30 }, (x) => '.')
   );
   let headPositions = [[0, 0]];
   let tailPositions = [
@@ -57,9 +86,7 @@ function solution(input) {
 
     for (let i = 0; i < value; i++) {
       headPositions.push(headMovement[direction](headPositions.at(-1)));
-
       let currentHead = headPositions.at(-1);
-
       tailPositions.forEach((tail, i) => {
         let currentTail = tail.at(-1);
         if (!i) {
@@ -70,7 +97,7 @@ function solution(input) {
             currentTail[1]
           );
           if (distance == 2 || distance == 2.23606797749979) {
-            tail.push(tailMovement[direction](currentHead));
+            tail.push(headTailMovement[direction](currentHead));
             if (
               !differentTailPossitions[i].some(
                 (x) => x[0] == tail.at(-1)[0] && x[1] == tail.at(-1)[1]
@@ -79,9 +106,9 @@ function solution(input) {
               differentTailPossitions[i].push(tail.at(-1));
           }
         } else {
-          //The reference is lost when one of the knots get to a corner.
-          //You need to maintain the reference differently
-          //This works differently. Refer to the problem to see how it behaves
+          //This is good because when this happens is when it needs to change
+
+          //This new solutions goes well until a certain point, it goes wrong on i > 2
           let distance = calculateDistance(
             tailPositions[i - 1].at(-1)[0],
             currentTail[0],
@@ -89,7 +116,16 @@ function solution(input) {
             currentTail[1]
           );
           if (distance == 2 || distance == 2.23606797749979) {
-            tail.push(tailMovement[direction](tailPositions[i - 1].at(-1)));
+            tail.push(
+              knotMovement[direction](
+                tailPositions[i - 1].at(-2),
+                currentTail,
+                distance
+              )
+            );
+            //Something is wrong you need to draw it by hand to see how it SHOULD behave
+            if (i == 2 || i == 1)
+              arraySim[tail.at(-1)[1] + 14][tail.at(-1)[0] + 14] = '#';
             if (
               !differentTailPossitions[i].some(
                 (x) => x[0] == tail.at(-1)[0] && x[1] == tail.at(-1)[1]
@@ -102,7 +138,9 @@ function solution(input) {
     }
   });
 
-  return tailPositions;
+  for (let i = 0; i < arraySim.length; i++) console.log(...arraySim[i]);
+
+  return;
 }
 // console.log(solution(input));
 console.log(solution(testInput));
