4c17df9726e96038ff19c7308f2f6441aa8cd404
Jalinson Diaz <zjaaaldev@gmail.com>
2022-12-10 12:32:40 -0300

    solved day 10

--- adventOfCode/day10/part1/solution.js
+++ adventOfCode/day10/part1/solution.js
@@ -7,9 +7,42 @@ Find the signal strength during the
 20th, 60th, 100th, 140th, 180th, and 220th cycles.
 What is the sum of these six signal strengths?
  */
-
+let noop = 1;
+let addx = 2;
 function solution(input) {
-  return input;
+  let cycle = 0;
+  let result = 1;
+  let inputArray = input.split('\n');
+
+  let currentMilestone = 20;
+  let resultArray = [];
+
+  for (let i = 0; i < inputArray.length; i++) {
+    let cmd = inputArray[i];
+    // console.log(cmd);
+    // console.log(cycle, cmd);
+    let cmdArray = cmd.split(' ');
+    if (cmdArray[0] == 'noop') {
+      if (cycle == currentMilestone) {
+        resultArray.push(result * cycle);
+        currentMilestone += 40;
+      }
+      cycle += noop;
+    }
+    if (cmdArray[0] == 'addx') {
+      for (let i = 0; i < 2; i++) {
+        ++cycle;
+        if (cycle == currentMilestone) {
+          // console.log(cycle, result);
+          resultArray.push(result * cycle);
+          currentMilestone += 40;
+        }
+        if (i == 1) result += Number(cmdArray[1]);
+      }
+    }
+  }
+
+  return resultArray.reduce((acc, curr) => (acc += curr));
 }
 
 console.log(solution(input));
--- adventOfCode/day10/part2/solution.js
+++ adventOfCode/day10/part2/solution.js
@@ -3,14 +3,125 @@ console.clear();
 console.log('----------------------------------');
 
 /*
-Find the signal strength during the
-20th, 60th, 100th, 140th, 180th, and 220th cycles.
-What is the sum of these six signal strengths?
+This is crazy. But I know how to do it
  */
-
 function solution(input) {
-  return input;
+  let cycle = 0;
+  let result = 1;
+  let CRTResult = [];
+  let inputArray = input.split('\n');
+  let currentCRT = Array.from({ length: 40 }, (_) => '#');
+  let pointer = 0;
+  let sprite = [
+    '#',
+    '#',
+    '#',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+  ];
+  let currentMilestone = 40;
+
+  for (let i = 0; i < inputArray.length; i++) {
+    let cmd = inputArray[i];
+    let cmdArray = cmd.split(' ');
+    if (cmdArray[0] == 'noop') {
+      // console.log(cycle, currentMilestone);
+      if (cycle == currentMilestone) {
+        currentMilestone += 40;
+        pointer = 0;
+        CRTResult.push(currentCRT);
+        currentCRT = Array.from({ length: 40 }, (_) => '#');
+      }
+
+      // if (cycle > 199)
+      //   console.log(
+      //     cycle,
+      //     currentCRT[pointer],
+      //     sprite[pointer],
+      //     currentCRT[pointer] == '#',
+      //     sprite[pointer] == '#'
+      //   );
+
+      if (sprite[pointer] == '#' && currentCRT[pointer] == '#')
+        currentCRT[pointer] = '#';
+      else currentCRT[pointer] = '.';
+
+      ++cycle;
+      ++pointer;
+    }
+    if (cmdArray[0] == 'addx') {
+      for (let i = 0; i < 2; i++) {
+        if (cycle == currentMilestone) {
+          currentMilestone += 40;
+          pointer = 0;
+          CRTResult.push(currentCRT);
+          currentCRT = Array.from({ length: 40 }, (_) => '#');
+        }
+        // if (cycle > 199)
+        //   console.log(
+        //     cycle,
+        //     currentCRT[pointer],
+        //     sprite[pointer],
+        //     currentCRT[pointer] == '#',
+        //     sprite[pointer] == '#'
+        //   );
+
+        if (sprite[pointer] == '#' && currentCRT[pointer] == '#')
+          currentCRT[pointer] = '#';
+        else currentCRT[pointer] = '.';
+
+        if (i == 1) {
+          result += Number(cmdArray[1]);
+          sprite = sprite
+            .map((x) => (x == '#' ? '.' : x))
+            .map((x, i) => {
+              if (i == result - 1 || i == result || i == result + 1) return '#';
+              else return '.';
+            });
+        }
+        ++cycle;
+        ++pointer;
+      }
+    }
+    if (cycle == 240) CRTResult.push(currentCRT);
+  }
+  return CRTResult;
 }
 
-console.log(solution(input));
+console.log(JSON.stringify(solution(input)));
 // console.log(solution(testInput));
