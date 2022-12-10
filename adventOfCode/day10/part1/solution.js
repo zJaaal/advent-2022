39c230637ac11965a7711ccc9a74a37339e5ba74
Jalinson Diaz <zjaaaldev@gmail.com>
2022-12-10 03:15:13 -0300

    day 10 template

---
+++ adventOfCode/day10/part1/solution.js
@@ -0,0 +1,16 @@
+import { input, testInput } from '../input.js';
+console.clear();
+console.log('----------------------------------');
+
+/*
+Find the signal strength during the
+20th, 60th, 100th, 140th, 180th, and 220th cycles.
+What is the sum of these six signal strengths?
+ */
+
+function solution(input) {
+  return input;
+}
+
+console.log(solution(input));
+// console.log(solution(testInput));
