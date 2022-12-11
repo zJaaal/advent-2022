6f2fb264b68dba828acd7a4e3a9ddd311442855c
Jalinson Diaz <zjaaaldev@gmail.com>
2022-12-11 19:24:03 -0300

    fixed day 8

--- adventJS/day_8.js
+++ adventJS/day_8.js
@@ -1,4 +1,5 @@
 // Hacky solution
+// Midu invalidated this solution
 // function checkPart(part) {
 //   if (new Set([...part.split('')]).size == part.length) return false;
 //   return [0, 2, part.length].includes(
@@ -9,14 +10,21 @@
 //   );
 // }
 
-// Trying to fix it
+// Idea extracted from https://github.com/RicardoxDev/adventJS/blob/main/2022/challenge-08/app.js 180 points
+// But optimized 260 points
 function checkPart(part) {
-  let partArray = part.split('');
-  return [0, 1].includes(
-    partArray.splice(0, part.length / 2).reduce((acc, curr, i) => {
-      if (partArray.includes(curr) == i) acc.push(curr);
-      return acc;
-    }, []).length
+  const reverse = (arr) => arr.reverse().join('');
+  part = part.split('');
+  let oldLetter = '';
+
+  return part.some(
+    (letter, i, result) => (
+      (oldLetter = letter),
+      (part[i] = ''),
+      (result = reverse(part) === reverse(part)),
+      !result && (part[i] = oldLetter),
+      result
+    )
   );
 }
 console.log(checkPart('ymolomoloy')); // > true (should be false)
