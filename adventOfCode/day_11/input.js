a2a5500c060dc973c406d3f6988305453e819db0
Jalinson Diaz <zjaaaldev@gmail.com>
2022-12-11 18:20:00 -0300

    solved part 1 day 11

--- adventOfCode/day_11/input.js
+++ adventOfCode/day_11/input.js
@@ -1,287 +1,153 @@
-export const input = `addx 1
-addx 4
-addx -2
-addx 3
-addx 3
-addx 1
-noop
-addx 5
-noop
-noop
-noop
-addx 5
-addx 2
-addx 3
-noop
-addx 2
-addx 4
-noop
-addx -1
-noop
-addx 3
-addx -10
-addx -17
-noop
-addx -3
-addx 2
-addx 25
-addx -24
-addx 2
-addx 5
-addx 2
-addx 3
-noop
-addx 2
-addx 14
-addx -9
-noop
-addx 5
-noop
-noop
-addx -2
-addx 5
-addx 2
-addx -5
-noop
-noop
-addx -19
-addx -11
-addx 5
-addx 3
-noop
-addx 2
-addx 3
-addx -2
-addx 2
-noop
-addx 3
-addx 4
-noop
-noop
-addx 5
-noop
-noop
-noop
-addx 5
-addx -3
-addx 8
-noop
-addx -15
-noop
-addx -12
-addx -9
-noop
-addx 6
-addx 7
-addx -6
-addx 4
-noop
-noop
-noop
-addx 4
-addx 1
-addx 5
-addx -11
-addx 29
-addx -15
-noop
-addx -12
-addx 17
-addx 7
-noop
-noop
-addx -32
-addx 3
-addx -8
-addx 7
-noop
-addx -2
-addx 5
-addx 2
-addx 6
-addx -8
-addx 5
-addx 2
-addx 5
-addx 17
-addx -12
-addx -2
-noop
-noop
-addx 7
-addx 9
-addx -8
-addx 2
-addx -33
-addx -1
-addx 2
-noop
-addx 26
-addx -22
-addx 19
-addx -16
-addx 8
-addx -1
-addx 3
-addx -2
-addx 2
-addx -17
-addx 24
-addx 1
-noop
-addx 5
-addx -1
-noop
-addx 5
-noop
-noop
-addx 1
-noop
-noop`;
+export const input = [
+  {
+    items: [71, 86],
+    operation: (old) => Math.floor((old * 13) / 3),
+    test: (value) => (value % 19 == 0 ? 6 : 7),
+    inspections: 0,
+  },
+  {
+    items: [66, 50, 90, 53, 88, 85],
+    operation: (old) => Math.floor((old + 3) / 3),
+    test: (value) => (value % 2 == 0 ? 5 : 4),
+    inspections: 0,
+  },
+  {
+    items: [97, 54, 89, 62, 84, 80, 63],
+    operation: (old) => Math.floor((old + 6) / 3),
+    test: (value) => (value % 13 == 0 ? 4 : 1),
+    inspections: 0,
+  },
+  {
+    items: [82, 97, 56, 92],
+    operation: (old) => Math.floor((old + 2) / 3),
+    test: (value) => (value % 5 == 0 ? 6 : 0),
+    inspections: 0,
+  },
+  {
+    items: [50, 99, 67, 61, 86],
+    operation: (old) => Math.floor((old * old) / 3),
+    test: (value) => (value % 7 == 0 ? 5 : 3),
+    inspections: 0,
+  },
+  {
+    items: [61, 66, 72, 55, 64, 53, 72, 63],
+    operation: (old) => Math.floor((old + 4) / 3),
+    test: (value) => (value % 11 == 0 ? 3 : 0),
+    inspections: 0,
+  },
+  {
+    items: [59, 79, 63],
+    operation: (old) => Math.floor((old * 7) / 3),
+    test: (value) => (value % 17 == 0 ? 2 : 7),
+    inspections: 0,
+  },
+  {
+    items: [55],
+    operation: (old) => Math.floor((old + 7) / 3),
+    test: (value) => (value % 3 == 0 ? 2 : 1),
+    inspections: 0,
+  },
+];
 
-export const testInput = `addx 15
-addx -11
-addx 6
-addx -3
-addx 5
-addx -1
-addx -8
-addx 13
-addx 4
-noop
-addx -1
-addx 5
-addx -1
-addx 5
-addx -1
-addx 5
-addx -1
-addx 5
-addx -1
-addx -35
-addx 1
-addx 24
-addx -19
-addx 1
-addx 16
-addx -11
-noop
-noop
-addx 21
-addx -15
-noop
-noop
-addx -3
-addx 9
-addx 1
-addx -3
-addx 8
-addx 1
-addx 5
-noop
-noop
-noop
-noop
-noop
-addx -36
-noop
-addx 1
-addx 7
-noop
-noop
-noop
-addx 2
-addx 6
-noop
-noop
-noop
-noop
-noop
-addx 1
-noop
-noop
-addx 7
-addx 1
-noop
-addx -13
-addx 13
-addx 7
-noop
-addx 1
-addx -33
-noop
-noop
-noop
-addx 2
-noop
-noop
-noop
-addx 8
-noop
-addx -1
-addx 2
-addx 1
-noop
-addx 17
-addx -9
-addx 1
-addx 1
-addx -3
-addx 11
-noop
-noop
-addx 1
-noop
-addx 1
-noop
-noop
-addx -13
-addx -19
-addx 1
-addx 3
-addx 26
-addx -30
-addx 12
-addx -1
-addx 3
-addx 1
-noop
-noop
-noop
-addx -9
-addx 18
-addx 1
-addx 2
-noop
-noop
-addx 9
-noop
-noop
-noop
-addx -1
-addx 2
-addx -37
-addx 1
-addx 3
-noop
-addx 15
-addx -21
-addx 22
-addx -6
-addx 1
-noop
-addx 2
-addx 1
-noop
-addx -10
-noop
-noop
-addx 20
-addx 1
-addx 2
-addx 2
-addx -6
-addx -11
-noop
-noop
-noop`;
+export const part2Input = [
+  {
+    items: [71, 86],
+    operation: (old) => Math.floor(old * 13),
+    test: (value) => (value % 19 == 0 ? 6 : 7),
+    inspections: 0,
+  },
+  {
+    items: [66, 50, 90, 53, 88, 85],
+    operation: (old) => Math.floor(old + 3),
+    test: (value) => (value % 2 == 0 ? 5 : 4),
+    inspections: 0,
+  },
+  {
+    items: [97, 54, 89, 62, 84, 80, 63],
+    operation: (old) => Math.floor(old + 6),
+    test: (value) => (value % 13 == 0 ? 4 : 1),
+    inspections: 0,
+  },
+  {
+    items: [82, 97, 56, 92],
+    operation: (old) => Math.floor(old + 2),
+    test: (value) => (value % 5 == 0 ? 6 : 0),
+    inspections: 0,
+  },
+  {
+    items: [50, 99, 67, 61, 86],
+    operation: (old) => Math.floor(old * old),
+    test: (value) => (value % 7 == 0 ? 5 : 3),
+    inspections: 0,
+  },
+  {
+    items: [61, 66, 72, 55, 64, 53, 72, 63],
+    operation: (old) => Math.floor(old + 4),
+    test: (value) => (value % 11 == 0 ? 3 : 0),
+    inspections: 0,
+  },
+  {
+    items: [59, 79, 63],
+    operation: (old) => Math.floor(old * 7),
+    test: (value) => (value % 17 == 0 ? 2 : 7),
+    inspections: 0,
+  },
+  {
+    items: [55],
+    operation: (old) => Math.floor(old + 7),
+    test: (value) => (value % 3 == 0 ? 2 : 1),
+    inspections: 0,
+  },
+];
+export const testInput = [
+  {
+    items: [79, 98],
+    operation: (old) => Math.floor((old * 19) / 3),
+    test: (value) => (value % 23 == 0 ? 2 : 3),
+    inspections: 0,
+  },
+  {
+    items: [54, 65, 75, 74],
+    operation: (old) => Math.floor((old + 6) / 3),
+    test: (value) => (value % 19 == 0 ? 2 : 0),
+    inspections: 0,
+  },
+  {
+    items: [79, 60, 97],
+    operation: (old) => Math.floor((old * old) / 3),
+    test: (value) => (value % 13 == 0 ? 1 : 3),
+    inspections: 0,
+  },
+  {
+    items: [74],
+    operation: (old) => Math.floor((old + 3) / 3),
+    test: (value) => (value % 17 == 0 ? 0 : 1),
+    inspections: 0,
+  },
+];
+export const part2TestInput = [
+  {
+    items: [79, 98],
+    operation: (old) => old * 19,
+    test: (value) => (value % 23 == 0 ? 2 : 3),
+    inspections: 0,
+  },
+  {
+    items: [54, 65, 75, 74],
+    operation: (old) => old + 6,
+    test: (value) => (value % 19 == 0 ? 2 : 0),
+    inspections: 0,
+  },
+  {
+    items: [79, 60, 97],
+    operation: (old) => old ** 2,
+    test: (value) => (value % 13 == 0 ? 1 : 3),
+    inspections: 0,
+  },
+  {
+    items: [74],
+    operation: (old) => old + 3,
+    test: (value) => (value % 17 == 0 ? 0 : 1),
+    inspections: 0,
+  },
+];
--- adventOfCode/day_11/part1/solution.js
+++ adventOfCode/day_11/part1/solution.js
@@ -6,8 +6,24 @@ console.log('----------------------------------');
 Figure out which monkeys to chase by counting how many items they inspect over 20 rounds.
 What is the level of monkey business after 20 rounds of stuff-slinging simian shenanigans?
  */
+
+let rounds = 20;
 function solution(input) {
-  return input;
+  for (let i = 0; i++ < rounds; ) {
+    input.forEach((monkey) => {
+      while (monkey.items.length) {
+        monkey.inspections += 1;
+        let newValue = monkey.operation(monkey.items.shift());
+        let monkeyIndex = monkey.test(newValue);
+        input[monkeyIndex].items.push(newValue);
+      }
+    });
+  }
+  let result = input
+    .map((x) => x.inspections)
+    .sort((x, y) => y - x)
+    .slice(0, 2);
+  return result[0] * result[1];
 }
 
 console.log(solution(input));
