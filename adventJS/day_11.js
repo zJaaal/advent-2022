22d0ae227cc12ca0672e1e058749c8dc4b56b5f4
Jalinson Diaz <zjaaaldev@gmail.com>
2022-12-11 18:20:10 -0300

    solved adjs day 11

--- adventJS/day_11.js
+++ adventJS/day_11.js
@@ -1,3 +1,19 @@
 function getCompleted(part, total) {
-  return '1/1';
+  //Euclid Algorithm
+  let reduceFraction = (n, d) => {
+    let gcd = (a, b) => (b ? gcd(b, a % b) : a);
+    gcd = gcd(n, d);
+    return `${n / gcd}/${d / gcd}`;
+  };
+
+  let [partSeconds, totalSeconds] = [part, total].map(
+    (time) =>
+      time
+        .split(':')
+        .reduce((seconds, time, i) => seconds + time * 60 ** (2 - i), 0) //This reduce is clever, was not my idea
+  );
+  let result = reduceFraction(partSeconds, totalSeconds);
+  return result;
 }
+
+console.log(getCompleted('03:30:30', '05:50:50'));
