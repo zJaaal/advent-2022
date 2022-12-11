6ca55949997b45ff42258fb65fa0591e8c475a50
Jalinson Diaz <zjaaaldev@gmail.com>
2022-12-11 18:37:54 -0300

    day 11 part 2 solved

--- adventOfCode/day_11/part2/solution.js
+++ adventOfCode/day_11/part2/solution.js
@@ -1,13 +1,30 @@
-import { input, testInput } from '../input.js';
+import { part2Input as input, part2TestInput as testInput } from '../input.js';
 console.clear();
 console.log('----------------------------------');
 
 /*
-Figure out which monkeys to chase by counting how many items they inspect over 20 rounds.
-What is the level of monkey business after 20 rounds of stuff-slinging simian shenanigans?
+Same as part 1 but now it doesn't divide by 3 and is 10000 rounds
  */
+
+let rounds = 10000;
 function solution(input) {
-  return input;
+  let superModule = 1;
+  input.forEach((monkey) => (superModule *= monkey.testNumber));
+  for (let i = 0; i++ < rounds; ) {
+    input.forEach((monkey, i) => {
+      while (monkey.items.length) {
+        monkey.inspections += 1;
+        let newValue = monkey.operation(monkey.items.shift() % superModule);
+        let monkeyIndex = monkey.test(newValue);
+        input[monkeyIndex].items.push(newValue);
+      }
+    });
+  }
+  let result = input
+    .map((x) => x.inspections)
+    .sort((x, y) => y - x)
+    .slice(0, 2);
+  return result[0] * result[1];
 }
 
 console.log(solution(input));
