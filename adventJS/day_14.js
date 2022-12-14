025e8b979f9cedcf6c4975aac2b0fdf6dda41564
Jalinson Diaz <zjaaaldev@gmail.com>
2022-12-14 17:07:44 -0300

    possible approach

--- adventJS/day_14.js
+++ adventJS/day_14.js
@@ -1,32 +1,24 @@
 function getOptimalPath(path) {
-  let traverseTree = (value, array, position, depth) => {
-    if (!array[depth]) return 0;
+  let result = new Set();
+  let traverseTree = (value, array, depth, position) => {
+    if (!array[depth]) {
+      console.log(value, depth);
+      result.add(value);
+      return;
+    }
 
-    let minIndex = array[depth].findIndex((x) => {
-      // console.log(array[depth - 1], position);
-      if (depth && position == array[depth - 1].length - 1) {
-        return (
-          x == Math.min(array[depth][position + 1], array[depth][position])
-        );
-      }
-      return (
-        x ==
-        Math.min(
-          array[depth][position - 1] || Infinity,
-          array[depth][position],
-          array[depth][position + 1] || Infinity
-        )
-      );
+    // console.log(array[depth]);
+
+    array[depth].forEach((cost, i) => {
+      //position
+
+      traverseTree(value + cost, array, depth + 1);
     });
-
-    // console.log(minIndex);
-
-    value = array[depth][minIndex];
-
-    return value + traverseTree(value, array, minIndex, depth + 1);
   };
 
-  return traverseTree(0, path, 0, 0);
+  traverseTree(0, path, 0, 0);
+  // console.log(result);
+  return Math.min(...result);
 }
 
 let path = [[0], [3, 4], [9, 8, 1]];
