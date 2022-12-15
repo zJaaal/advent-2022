ee21a91283c28c44e80abee445a52b2c2e00af15
Jalinson Diaz <zjaaaldev@gmail.com>
2022-12-15 16:43:21 -0300

    part 1 solved

---
+++ adventOfCode/day_15/part1/solution.js
@@ -0,0 +1,67 @@
+import { input, testInput } from '../input.js';
+console.clear();
+console.log('----------------------------------');
+
+/*
+Consult the report from the sensors you just deployed.
+In the row where y=2000000, how many positions cannot contain a beacon?
+ */
+
+function solution(input, rowToCheck) {
+  let inputArray = input.split('\n');
+  let sensors = [];
+  let beaconSetInRow = new Set();
+  inputArray.forEach((pairs) => {
+    let [sensor, beacon] = pairs.split(':');
+    let sensorCoors = sensor.match(/-?\d+/g).map((x) => +x);
+    let beaconCoors = beacon.match(/-?\d+/g).map((x) => +x);
+    sensors.push({
+      x: sensorCoors[0],
+      y: sensorCoors[1],
+      nearestBeacon: {
+        x: beaconCoors[0],
+        y: beaconCoors[1],
+      },
+      distance:
+        Math.abs(sensorCoors[0] - beaconCoors[0]) +
+        Math.abs(sensorCoors[1] - beaconCoors[1]),
+    });
+
+    if (beaconCoors[1] == rowToCheck)
+      beaconSetInRow.add(`${beaconCoors[0]}/${beaconCoors[1]}`);
+  });
+
+  sensors = sensors
+    .filter((sensor) => {
+      //To see if a sensor can touche the beacon
+
+      if (sensor.y == rowToCheck) return true;
+      if (sensor.y > rowToCheck)
+        return sensor.y - sensor.distance <= rowToCheck;
+      else return sensor.y + sensor.distance >= rowToCheck;
+    })
+    .map((sensor) => {
+      //This is the calculation to know how many rows does the signal touches
+      let distanceFromRow = Math.abs(rowToCheck - sensor.y);
+      let totalRowOccupy = sensor.distance * 2 - distanceFromRow * 2 + 1;
+
+      let maxXOcuppy = (totalRowOccupy - 1) / 2 + sensor.x;
+      let minXOcuppy = Math.abs(sensor.x) - (totalRowOccupy - 1) / 2;
+
+      return {
+        ...sensor,
+        minXOcuppation: minXOcuppy,
+        maxXOcuppation: maxXOcuppy,
+      };
+    });
+
+  let minXOcuppation = Math.min(...sensors.map((x) => x.minXOcuppation));
+  let maxXOcuppation = Math.max(...sensors.map((x) => x.maxXOcuppation));
+
+  let totalSpaces = Math.abs(minXOcuppation) + maxXOcuppation + 1;
+
+  return totalSpaces - beaconSetInRow.size;
+}
+
+// console.log(solution(input, 2000000));
+console.log(solution(testInput, 10));
