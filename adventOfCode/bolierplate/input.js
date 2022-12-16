25f75827fcfad55943bbb660c55a762531ed70d7
Jalinson Diaz <zjaaaldev@gmail.com>
2022-12-16 12:30:09 -0300

    added bolierplate

---
+++ adventOfCode/bolierplate/input.js
@@ -0,0 +1,3 @@
+export const input = ``;
+
+export const testInput = ``;
---
+++ adventOfCode/bolierplate/part1/solution.js
@@ -0,0 +1,14 @@
+import { input, testInput } from '../input.js';
+console.clear();
+console.log('----------------------------------');
+
+/*
+ Add useful description
+ */
+
+function solution(input) {
+  return input;
+}
+
+console.log(solution(input));
+// console.log(solution(testInput));
