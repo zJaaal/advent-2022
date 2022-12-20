d47e02131546f7c387b42a47317390ffcea05aa6
Jalinson Diaz <zjaaaldev@gmail.com>
2022-12-20 16:06:23 -0300

    deleted object.values

--- adventJS/day_20.js
+++ adventJS/day_20.js
@@ -1,10 +1,11 @@
 function howManyReindeers(reindeerTypes, gifts) {
   return gifts.map((gift) => {
     let currentMax = gift.weight;
-    let weights = Object.values(reindeerTypes)
+    let weights = reindeerTypes
       .map((x) => [x.type, x.weightCapacity])
       .filter((x) => x[1] < currentMax)
       .sort((x, y) => y[1] - x[1]);
+
     let currentCapacity = weights.reduce((acc, curr) => (acc += curr[1]), 0);
 
     let reindeerQuantity = weights.map(([type, weight]) => {
